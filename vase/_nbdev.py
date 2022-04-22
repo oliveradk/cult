86bf5f0d2e85aa71c5e1f791f5770d2e8d4a3e31
oliveradk <danielskoch.oliver@gmail.com>
2022-04-22 19:31:15 -0400

    models

--- vase/_nbdev.py
+++ vase/_nbdev.py
@@ -3,9 +3,18 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"PARENT_DIR": "00b_config.ipynb",
-         "DATA_PATH": "00b_config.ipynb"}
+         "DATA_PATH": "00b_config.ipynb",
+         "CommonMNIST": "01a_core.datasets.moving_mnist.ipynb",
+         "CommonFashionMNIST": "01a_core.datasets.moving_mnist.ipynb",
+         "MovingMNIST": "01a_core.datasets.moving_mnist.ipynb",
+         "MovingFashionMNIST": "01a_core.datasets.moving_mnist.ipynb",
+         "Encoder": "02a_core.models.ipynb",
+         "EnvironmentInference": "02a_core.models.ipynb",
+         "env_dist_to_one_hot": "02a_core.models.ipynb"}
 
-modules = ["config.py"]
+modules = ["config.py",
+           "core/datasets/moving_mnist.py",
+           "core/models.py"]
 
 doc_url = "https://oliveradk.github.io/vase/"
 
